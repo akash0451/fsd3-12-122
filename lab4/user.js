291eeefd6747718e88ab147fb16aa6711c129830
akash0451 <akashakg05@gmail.com>
2026-09-11 15:43:59 +0530

    lab4

---
+++ lab4/user.js
@@ -0,0 +1,9 @@
+// we use in memory database
+let users = [
+  { id: 1, name: "Akash Gupta", mob: "6299xxx", email: "akashakg05@gamil.com" },
+  { id: 2, name: "Amit Sharma", mob: "817xxx", email: "amitakg05@gamil.com" },
+];
+
+let nextId = 3;
+
+export const getusers = () => users;
