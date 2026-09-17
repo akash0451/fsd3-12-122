b082802a74caf46fbb2f0d52bc997d0f29f6002f
akash0451 <akashakg05@gmail.com>
2026-09-17 12:21:53 +0530

    add user and get  user by id created

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,15 +1,27 @@
 import http from "http";
-import { getUsers} from "./user.js";
-
+import { getUsers} from"./user.js" ;
+import { addUser} from "./user.js";
 const server = http.createServer((req, res) => {
   if ((req.url === "/api/users" && req.method === "GET")) {
-    res.end(JSON.stringify({ msg: "all users" }));
+    res.end(JSON.stringify(getUsers()));
   } 
   else if ((req.url === "/api/users" && req.method === "POST")) {
-    res.end(JSON.stringify({ msg: "add user" }));
+    let body ='';
+    req.on('data',(chunk)=>{
+       body += chunk
+    })
+    req.on('end',()=>{
+      const user =JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated}));
+      
+    })
+    
   }
   else if ((req.url === "/api/users/1"&& req.method === "GET")) {
-    res.end(JSON.stringify({ msg: "single user with Id 1" }));
+
+    const userId =Number(req.url.split('/').pop())
+    res.end(JSON.stringify({ msg: `Showing details of user with id ${userId}` }));
   }
   else if ((req.url === "/api/users/1" && req.method === "PUT")) {
     res.end(JSON.stringify({ msg: "update user 1" }));
--- lab4/user.js
+++ lab4/user.js
@@ -6,4 +6,10 @@ let users = [
 
 let nextId = 3;
 
-export const getusers = () => users;
+export const getUsers = () => users;
+
+export const addUser=(user)=>{
+  user.id = nextId++;
+  users.push(user);
+  return users;
+}
